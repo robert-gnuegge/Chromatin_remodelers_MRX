00998dc80764e23ce12d268935bcb95c46416d2e
robert-gnuegge <robert.gnuegge@gmail.com>
2024-04-22 17:00:22 -0400

    Added anti-Mre11 ChIP-qPCR analysis scripts.

---
+++ ChIP-qPCR_Mre11/02_Scripts/02_Plot_averages.R
@@ -0,0 +1,146 @@
+# info --------------------------------------------------------------------
+# purpose: plot ChIP-qPCR data
+# author: Robert Gnuegge (robert.gnuegge@gmail.com)
+# created: 04/22/24
+# last modified: 04/22/24
+
+# read helper functions and files -----------------------------------------
+source(file = "../Src/JFly_colors.R")
+source(file = "../Src/Misc_helper_functions.R")
+
+
+# read data ---------------------------------------------------------------
+avg_data <- read.table(file = "03_Processed_data/percent_input_averages.txt", header = TRUE)
+avg_data$strain[avg_data$sample == "No Ab"] <- "No Ab"  # for easier data sorting
+
+raw_data <- read.table(file = "01_Raw_data/percent_input_replicates.txt", header = TRUE)
+raw_data$strain[raw_data$sample == "No Ab"] <- "No Ab"  # for easier data sorting
+
+
+# plotting function -------------------------------------------------------
+my_plot <- function(avg_data, raw_data, strains, colors, file_name, y_range = NULL, width=2.5, height=2.5){
+  
+  if(is.null(y_range)){
+    y_range <- range(c(avg_data$mean + avg_data$sd, avg_data$mean - avg_data$sd, raw_data$mean, 0), na.rm = TRUE)
+    y_range[1] <- y_range[1] - 0.02 * y_range[2]  # increase slightly to prevent partial plotting of data points at plot border
+    y_range[2] <- 1.02 * y_range[2]
+  }
+
+  # print to PDF
+  pdf(file = "tmp.pdf", width=width, height=height)
+  par(cex = 1, mar = c(5.1, 4.1, 4.1, 2.1) - c(2.5, 1.7, 4.1, 2.1), tcl = -0.3, mgp = c(1.5, 0.6, 0), las = 1)
+  
+  # prepare average data for plotting
+  avg_data$sort <- match(x = avg_data$strain, table = strains)
+  avg_data <- avg_data[order(avg_data$sort, avg_data$time), ]
+  heights <- matrix(data = avg_data$mean, nrow = length(strains), byrow = TRUE)
+  
+  # plot barplot
+  bp <- barplot(height = heights, beside = TRUE, ylim = y_range, xlab = "Time (h)", ylab = "% Input", col = colors)
+  axis(side = 1, at = apply(X = bp, MARGIN = 2, FUN = mean), labels = c(0, 1, 2, 4), line = -0.4, lwd = 0)
+  
+  # add individual data.points
+  x <- as.vector(t(bp))
+  x_shift <- min(diff(sort(x))) / 5
+  
+  raw_data$sort <- match(x = raw_data$strain, table = strains)
+  raw_data <- raw_data[order(raw_data$sort, raw_data$time), ]
+
+  points(x = x - x_shift, y = raw_data$mean[raw_data$replicate == 1], pch = 20, cex = 0.67, col = gray(level = 0.4))
+  points(x = x, y = raw_data$mean[raw_data$replicate == 2], pch = 20, cex = 0.67, col = gray(level = 0.4))
+  points(x = x + x_shift, y = raw_data$mean[raw_data$replicate == 3], pch = 20, cex = 0.67, col = gray(level = 0.4))
+  
+  # add error bars (sd)
+  arrows(x0 = x,
+         y0 = avg_data$mean - avg_data$sd,
+         x1 = x,
+         y1 = avg_data$mean + avg_data$sd,
+         length = 0.03, # length of arrow head
+         angle = 90, # angle of arrow head
+         code = 3 # to draw arrow head on both ends
+  )
+  
+  dev.off()
+  GS_embed_fonts(input = "tmp.pdf", output = file_name)
+  
+}
+
+
+# plotting ================================================================
+plot_dir <- "04_Plots/"
+
+strains <- c("4518-13B", "5415", "No Ab")
+MyColors <- c("gray", JFly_colors[2], "white")
+
+
+# plot legend -------------------------------------------------------------
+Legend_txt <- c("WT", expression(italic("fun30"*Delta)), "No Ab")
+
+pdf(file = "tmp.pdf", width=1.1, height=0.85)
+par(cex = 1, mar = rep(0, 4))
+plot(1, type="n", axes=FALSE, xlab="", ylab="")
+legend(1, 1, xjust=0.5, yjust=0.5, legend = Legend_txt, fill = MyColors)
+dev.off()
+GS_embed_fonts(input = "tmp.pdf", output = paste0(plot_dir, "Legend.pdf"))
+
+pdf(file = "tmp.pdf", width=3.0, height=0.45)
+par(cex = 1, mar = rep(0, 4))
+plot(1, type="n", axes=FALSE, xlab="", ylab="")
+legend(1, 1, xjust=0.5, yjust=0.5, legend = Legend_txt, fill = MyColors, ncol = 3)
+dev.off()
+GS_embed_fonts(input = "tmp.pdf", output = paste0(plot_dir, "Legend_horiz.pdf"))
+
+
+# +98 bp ----------------------------------------------------------------------
+avg <- avg_data[avg_data$strain %in% strains & avg_data$primers == "oRG50_oRG51", ]
+raw <- raw_data[raw_data$strain %in% strains & raw_data$primers == "oRG50_oRG51", ]
+
+my_plot(avg_data = avg, raw_data = raw, y_range = c(-0.1, 5.3),
+        strains = strains, colors = MyColors, file_name = paste0(plot_dir, "Percent_input_98_bp.pdf"))
+
+
+# +640 bp ---------------------------------------------------------------------
+avg <- avg_data[avg_data$strain %in% strains & avg_data$primers == "oRG52_oRG53", ]
+raw <- raw_data[raw_data$strain %in% strains & raw_data$primers == "oRG52_oRG53", ]
+
+my_plot(avg_data = avg, raw_data = raw, y_range = c(-0.1, 5.3),
+        strains = strains, colors = MyColors, file_name = paste0(plot_dir, "Percent_input_647_bp.pdf"))
+
+
+
+# ADH1 --------------------------------------------------------------------
+avg <- avg_data[avg_data$strain %in% strains & avg_data$primers == "oRG54_oRG55", ]
+raw <- raw_data[raw_data$strain %in% strains & raw_data$primers == "oRG54_oRG55", ]
+
+my_plot(avg_data = avg, raw_data = raw, y_range = c(-0.1, 5.3),
+        strains = strains, colors = MyColors, file_name = paste0(plot_dir, "Percent_input_ADH1.pdf"))
+
+
+# compact =======================================================================
+strains <- c("4518-13B", "5415")
+MyColors <- c("gray", JFly_colors[2])
+
+
+# plot legend -------------------------------------------------------------
+Legend_txt <- c("WT", expression(italic("fun30"*Delta)))
+
+pdf(file = "tmp.pdf", width=1.1, height=0.65)
+par(cex = 1, mar = rep(0, 4))
+plot(1, type="n", axes=FALSE, xlab="", ylab="")
+legend(1, 1, xjust=0.5, yjust=0.5, legend = Legend_txt, fill = MyColors)
+dev.off()
+GS_embed_fonts(input = "tmp.pdf", output = paste0(plot_dir, "Legend_WT_fun30.pdf"))
+
+pdf(file = "tmp.pdf", width=2.05, height=0.45)
+par(cex = 1, mar = rep(0, 4))
+plot(1, type="n", axes=FALSE, xlab="", ylab="")
+legend(1, 1, xjust=0.5, yjust=0.5, legend = Legend_txt, fill = MyColors, ncol = 2)
+dev.off()
+GS_embed_fonts(input = "tmp.pdf", output = paste0(plot_dir, "Legend_horiz_WT_fun30.pdf"))
+
+# +98 bp ----------------------------------------------------------------------
+avg <- avg_data[avg_data$strain %in% strains & avg_data$primers == "oRG50_oRG51", ]
+raw <- raw_data[raw_data$strain %in% strains & raw_data$primers == "oRG50_oRG51", ]
+
+my_plot(avg_data = avg, raw_data = raw, y_range = c(-0.1, 5.3), width = 2, height = 2,
+        strains = strains, colors = MyColors, file_name = paste0(plot_dir, "Percent_input_98_bp_WT_fun30.pdf"))
