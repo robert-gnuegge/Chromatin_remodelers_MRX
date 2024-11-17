a0958a4da9140499261088394b8b935f82a40646
robert-gnuegge <robert.gnuegge@gmail.com>
2024-11-17 12:01:34 +0100

    Modified script to plot seq data aligned at nucleosome.

--- S1-seq_and_MNase-seq/Rep_merged/Analyze_and_plot/02_Scripts/03_Plot_average_S1-seq_spreading_aligned_at_nuc.R
+++ S1-seq_and_MNase-seq/Rep_merged/Analyze_and_plot/02_Scripts/03_Plot_average_S1-seq_spreading_aligned_at_nuc.R
@@ -2,7 +2,7 @@
 # purpose: align MNase-seq and S1-seq data at DSB-proximal nucleosome, average and plot
 # author: Robert Gnuegge (robert.gnuegge@gmail.com)
 # created: 05/27/24
-# last modified: 11/15/24
+# last modified: 11/16/24
 
 
 # load libraries ----------------------------------------------------------
@@ -17,67 +17,50 @@ source(file = "../../../Src/S_cerevisiae_SrfI_cut_sites.R")
 
 # function definitions ----------------------------------------------------
 
-add_nuc_numbers_and_strand <- function(DSBs, nuc_centers, ideal_nuc_centers){
-  tmp <- nuc_centers
-  roi <- flank(x = DSBs, width = 3000, start = TRUE) 
-  tmp_up <- subsetByIntersect(subject = tmp, query = roi)
-  strand(tmp_up) <- "-"
+# add distance to specified nucleosome
+# arguments: GRanges and numeric
+# result: GRanges with added mcols "dist_to_nuc" and "chosen_nuc" = "nuc"
+add_distance_to_nuc <- function(seq_data, nuc_pos, nuc = 1, nuc_dist = 165){
   
-  roi <- flank(x = DSBs, width = 3000, start = FALSE) 
-  tmp_dn <- subsetByIntersect(subject = tmp, query = roi)
-  strand(tmp_dn) <- "+"
-  tmp <- sort(c(tmp_up, tmp_dn), ignore.strand = TRUE)
+  # determine if data are on fw or rev strand (needed for distance calculation etc.)
+  plus_minus <- ifelse(test = as.character(unique(strand(nuc_pos))) == "+", yes = 1, no = -1)
   
-  hits <- distanceToNearest(x = tmp, subject = ideal_nuc_centers)
-  mcols(tmp)$nuc_number <- NA
-  mcols(tmp[queryHits(hits)])$nuc_number <- mcols(ideal_nuc_centers[subjectHits(hits)])$nuc_number
-  return(tmp)
-}
-
-add_distance_to_DSB_prox_nuc <- function(seq_data, nuc_pos){
-  out <- GRanges()
-  for(n in 1:length(roi)){
-    nuc_pos_roi <- subsetByOverlaps(x = nuc_pos, ranges = roi[n])
-    algnmt_nuc <- nuc_pos_roi[which.min(abs(nuc_pos_roi$idx) - algnmt_nuc_idx)]
-    # take next nucleosome in case no algnment_nuc_idx was identified
-    if(length(algnmt_nuc) > 1){
-      # use mean of coordinates in case several nucleosomes have the chosen idx
-      tmp <- round(mean(start(algnmt_nuc)))
-      algnmt_nuc <- algnmt_nuc[1]
-      start(algnmt_nuc) <- tmp
+  # sometimes the same idx is assigned to several nucs
+  # in this case merge nucs and assign mean coordinate
+  if(any(duplicated(nuc_pos$idx))){
+    duplicates <- unique(nuc_pos$idx[duplicated(nuc_pos$idx)])
+    for(d in duplicates){
+      tmp <- nuc_pos[nuc_pos$idx == d]
+      coord <- round(mean(start(tmp)))
+      tmp <- tmp[1]
+      ranges(tmp) <- IRanges(start = coord, width = 1)
+      nuc_pos <- c(tmp, nuc_pos[nuc_pos$idx != d])
     }
-    tmp <- subsetByOverlaps(x = GRanges, ranges = roi[n])
-    if(strand(algnmt_nuc) == "-"){
-      tmp$dist_from_nuc <- start(algnmt_nuc) - start(tmp) + (abs(algnmt_nuc$idx) - algnmt_nuc_idx) * nuc_dist
-    }else{
-      tmp$dist_from_nuc <- start(tmp) - start(algnmt_nuc) + (algnmt_nuc$idx - algnmt_nuc_idx) * nuc_dist
-    }
-    tmp$algnmt_nuc <- nuc_pos$algnmt_nuc 
-    out <- c(out, tmp)
+    nuc_pos <- sort(nuc_pos)
   }
-  return(out)
+  
+  # if "(-)nuc" cannot be found, add it placing it at the expected distance
+  # from the next available nuc
+  if(!(nuc %in% abs(nuc_pos$idx))){
+    picked_nuc <- nuc + 1
+    while(!(picked_nuc %in% abs(nuc_pos$idx))){
+      picked_nuc <- picked_nuc + 1
+      if(picked_nuc - nuc > 3){
+        stop("No nucleosome in reasonable vicinity could be identified.")
+      }
+    }
+    tmp <- nuc_pos[1]
+    tmp$idx <- plus_minus * nuc
+    coord <- start(nuc_pos[abs(nuc_pos$idx) == picked_nuc]) - plus_minus * (picked_nuc - 1) * nuc_dist
+    ranges(tmp) <- IRanges(start = coord, width = 1)
+    nuc_pos <- sort(c(tmp, nuc_pos))
+  }
+  
+  seq_data$dist_to_nuc <- plus_minus * (start(seq_data) - start(nuc_pos[abs(nuc_pos$idx) == nuc]))
+  seq_data$chosen_nuc <- plus_minus * nuc
+  return(seq_data)
+  
 }
-
-# # function to find DSB proximal DSBs
-# find_DSB_prox_nuc <- function(DSB, nuc_pos_GRanges){
-#   
-#   roi <- flank(x = DSB, width = 3000, start = TRUE) 
-#   tmp <- subsetByIntersect(subject = nuc_pos_GRanges, query = roi)
-#   idx <- nearest(x = DSB, subject = tmp)
-#   up_prox_nuc <- granges(tmp[idx])
-#   mcols(up_prox_nuc) <- data.frame(distance = distance(x = DSB, y = up_prox_nuc), upstream = TRUE)
-#   
-#   roi <- flank(x = DSB, width = 3000, start = FALSE) 
-#   tmp <- subsetByIntersect(subject = nuc_pos_GRanges, query = roi)
-#   idx <- nearest(x = DSB, subject = tmp)
-#   dn_prox_nuc <- granges(tmp[idx])
-#   mcols(dn_prox_nuc) <- data.frame(distance = distance(x = DSB, y = dn_prox_nuc), upstream = FALSE)
-#   
-#   prox_nuc <- sort(x = c(up_prox_nuc, dn_prox_nuc))
-#   
-#   return(prox_nuc)
-#   
-# }
 
 # dirs --------------------------------------------------------------------
 plot_dir <- "04_Plots/MRE11/Avgs_algned_at_DSB_prxml_nuc"
@@ -108,48 +91,59 @@ LSY4518_13B_1h_nucleosome_positions <- subsetByIntersect(subject = LSY4518_13B_1
 LSY4518_13B_2h_nucleosome_positions <- subsetByIntersect(subject = LSY4518_13B_2h_nucleosome_positions, query = roi)
 LSY4518_13B_4h_nucleosome_positions <- subsetByIntersect(subject = LSY4518_13B_4h_nucleosome_positions, query = roi)
 
-LSY4518_13B_0h_nucleosome_positions[abs(LSY4518_13B_0h_nucleosome_positions$idx) == 1][1:10]
-LSY4518_13B_0h_nucleosome_positions[abs(LSY4518_13B_0h_nucleosome_positions$idx) == 1][11:20]
-LSY4518_13B_0h_nucleosome_positions[abs(LSY4518_13B_0h_nucleosome_positions$idx) == 1][21:30]
-LSY4518_13B_0h_nucleosome_positions[abs(LSY4518_13B_0h_nucleosome_positions$idx) == 1][31:38]
 
 # add distance to DSB-proximal nucleosome ---------------------------------
 
-# t = 0
-nuc <- 1
-MNase <- add_distance_to_nucleosome(GRanges = LSY4518_13B_0h_MNase_seq, nuc_centers = LSY4518_13B_0h_nucleosome_positions, roi = roi, nuc = nuc)
-S1 <- add_distance_to_nucleosome(GRanges = LSY4518_13B_0h_S1_seq, nuc_centers = LSY4518_13B_0h_nucleosome_positions, roi = roi, nuc = nuc)
-idx <- S1$dist_from_nuc > -(nuc - 0.5) * nuc_dist  # & S1$dist_from_nuc <= 1200
-S1_agg_0 <- aggregate(score ~ dist_from_nuc, data = S1[idx], FUN = mean)
-MNase_agg_0 <- aggregate(score ~ dist_from_nuc, data = MNase[idx], FUN = mean)
+# helper function (using the same nuc_pos object [t=0] for all seq_data objects)
+add_distance_to_nuc_helper <- function(object_name){
+  tmp <- GRanges()
+  for(r in 1:length(roi)){
+    seq_data <- subsetByIntersect(subject = get(object_name), query = roi[r])
+    nuc_pos <- subsetByIntersect(subject = LSY4518_13B_0h_nucleosome_positions, query = roi[r])
+    tmp <- c(tmp, add_distance_to_nuc(seq_data = seq_data, nuc_pos = nuc_pos, nuc = 1))
+  }
+  assign(x = object_name, value = tmp, envir = .GlobalEnv)
+}
 
-# t = 1
-nuc <- 2
-MNase <- add_distance_to_nucleosome(GRanges = LSY4518_13B_1h_MNase_seq, nuc_centers = LSY4518_13B_1h_nucleosome_positions, roi = roi, nuc = nuc)
-S1 <- add_distance_to_nucleosome(GRanges = LSY4518_13B_1h_S1_seq, nuc_centers = LSY4518_13B_1h_nucleosome_positions, roi = roi, nuc = nuc)
-idx <- S1$dist_from_nuc > -(nuc - 0.5) * nuc_dist  # & S1$dist_from_nuc <= 1200
-S1_agg_1 <- aggregate(score ~ dist_from_nuc, data = S1[idx], FUN = mean)
-MNase_agg_1 <- aggregate(score ~ dist_from_nuc, data = MNase[idx], FUN = mean)
+add_distance_to_nuc_helper(object_name = "LSY4518_13B_0h_S1_seq")
+add_distance_to_nuc_helper(object_name = "LSY4518_13B_1h_S1_seq")
+add_distance_to_nuc_helper(object_name = "LSY4518_13B_2h_S1_seq")
+add_distance_to_nuc_helper(object_name = "LSY4518_13B_4h_S1_seq")
 
-# t = 2
-nuc <- 3
-MNase <- add_distance_to_nucleosome(GRanges = LSY4518_13B_2h_MNase_seq, nuc_centers = LSY4518_13B_2h_nucleosome_positions, roi = roi, nuc = nuc)
-S1 <- add_distance_to_nucleosome(GRanges = LSY4518_13B_2h_S1_seq, nuc_centers = LSY4518_13B_2h_nucleosome_positions, roi = roi, nuc = nuc)
-idx <- S1$dist_from_nuc > -(nuc - 0.5) * nuc_dist  # & S1$dist_from_nuc <= 1200
-S1_agg_2 <- aggregate(score ~ dist_from_nuc, data = S1[idx], FUN = mean)
-MNase_agg_2 <- aggregate(score ~ dist_from_nuc, data = MNase[idx], FUN = mean)
-
-# t = 4
-nuc <- 4
-MNase <- add_distance_to_nucleosome(GRanges = LSY4518_13B_4h_MNase_seq, nuc_centers = LSY4518_13B_4h_nucleosome_positions, roi = roi, nuc = nuc)
-S1 <- add_distance_to_nucleosome(GRanges = LSY4518_13B_4h_S1_seq, nuc_centers = LSY4518_13B_4h_nucleosome_positions, roi = roi, nuc = nuc)
-idx <- S1$dist_from_nuc > -(nuc - 0.5) * nuc_dist  # & S1$dist_from_nuc <= 1200
-S1_agg_4 <- aggregate(score ~ dist_from_nuc, data = S1[idx], FUN = mean)
-MNase_agg_4 <- aggregate(score ~ dist_from_nuc, data = MNase[idx], FUN = mean)
+add_distance_to_nuc_helper(object_name = "LSY4518_13B_0h_MNase_seq")
+add_distance_to_nuc_helper(object_name = "LSY4518_13B_1h_MNase_seq")
+add_distance_to_nuc_helper(object_name = "LSY4518_13B_2h_MNase_seq")
+add_distance_to_nuc_helper(object_name = "LSY4518_13B_4h_MNase_seq")
 
 
 # plotting ----------------------------------------------------------------
-k <- 51
+S1_seq_1 <- LSY4518_13B_0h_S1_seq
+MNase_seq_0 <- LSY4518_13B_0h_MNase_seq
+
+plotting_function <- function(S1_seq_1, S1_seq_2, S1_seq_4, 
+                              MNase_seq_0, MNase_seq_1, MNase_seq_2, MNase_seq_4,
+                              k = 51, xlim = NULL, ylim = NULL, 
+                              MNase_seq_cols = gray(level = 9:6 * 0.1), 
+                              S1_seq_cols = JFly_colors[c(1, 4, 5)],
+                              nuc_marks = 1:6, nuc_mark_col = gray(level = 0.4)){
+  
+  MNase_agg_0 <- aggregate(score ~ dist_to_nuc, data = MNase_seq_0, FUN = mean)
+  MNase_agg_0$score <- runmed(x = MNase_agg_0$score, k = k)
+  
+  plot_polygon <- function(MNase_seq, col){
+    polygon(x = c(MNase_seq$dist_from_nuc[1], MNase_seq$dist_from_nuc, MNase_seq$dist_from_nuc[length(MNase_seq$dist_from_nuc)]), 
+            y = c(0, y, 0), col = col, border = NA)
+  }
+  
+  
+  S1_agg_1 <- aggregate(score ~ dist_to_nuc, data = S1_seq_1, FUN = mean)
+  S1_agg_2 <- aggregate(score ~ dist_to_nuc, data = S1_seq_2, FUN = mean)
+  S1_agg_4 <- aggregate(score ~ dist_to_nuc, data = S1_seq_4, FUN = mean)
+  
+  
+  
+}
+
 
 pdf(file = "tmp.pdf", width=3, height=2.5)
 par(cex = 1, mar = rep(0,4), oma = rep(0, 4))
